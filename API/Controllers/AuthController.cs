08eb421943052d5daa6c636ea6dd05a0b3b82c9a
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-03-21 21:14:53 -0400

    initial migration

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[AllowAnonymous]
+	public class AuthController : ControllerBase
+	{
+		private readonly IUnitOfWork _uow;
+		private readonly ServerResponse _reponse;
+		public AuthController(IUnitOfWork unitOfWork)
+		{
+			_uow = unitOfWork;
+			_reponse = new ServerResponse();
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> LoginUser([FromBody] LoginUserDTO model)
+		{
+			try
+			{
+				if (!await _uow.AuthRepository.ConfirmUserExists(model.Username))
+				{
+					return Ok(_reponse.GetResponse(false, "Este usuario no existe o las credenciales son incorrectas!!"));
+				}
+
+				var rs = await _uow.AuthRepository.LoginUser(model);
+
+				return Ok(rs);
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+}
--- Infrastructure/Context/MainContext.cs
+++ Infrastructure/Context/MainContext.cs
@@ -11,15 +11,24 @@ namespace Infrastructure.Context
 	{
 		public MainContext(DbContextOptions options) : base(options)
 		{
+
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{ 
-			
+		{
+			//if(true)
+			//{
+			//	new Infrastructure.Helpers.EncryptHelper().CreatePasswordHash("admin01", out byte[] passwordHash, out byte[] passwordSalt);
+
+			//	modelBuilder.Entity<Usuario>().HasData(
+			//		new Usuario { Id = 1, Username = "admin", PasswordHash = passwordHash, PasswordSalt = passwordSalt, EsAdministrador = true, Estatus = true }
+			//	);
+			//}
 		}
 
 		public DbSet<Usuario> Usuarios { get; set; }
 		public DbSet<Permiso> Permisos { get; set; }
 		public DbSet<UsuarioPermiso> UsuarioPermisos { get; set; }
+
 	}
 }
