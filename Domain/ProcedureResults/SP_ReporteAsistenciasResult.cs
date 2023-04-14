1e0538b075a79784991ab68a50d80f43f8e514d6
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-04-14 01:34:01 -0400

    modify precedure resultset

---
+++ Domain/ProcedureResults/SP_ReporteAsistenciasResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ResultSetsModels
+{
+    public class SP_ReporteAsistenciasResult
+    {
+        public string Region { get; set; }
+        public string CategoriaAsistencia { get; set; }
+        public string TipoAsistencia { get; set; }
+        public int Total { get; set; }
+    }
+}
