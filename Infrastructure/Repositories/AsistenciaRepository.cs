ef3ce9aa6d8f1d487975b66dcb944912a3c349cf
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-05-17 13:00:47 -0400

    insert to excel ready

--- Infrastructure/Repositories/AsistenciaRepository.cs
+++ Infrastructure/Repositories/AsistenciaRepository.cs
@@ -1,4 +1,5 @@
-﻿using Domain.ProcedureResults;
+﻿using Domain.Entities;
+using Domain.ProcedureResults;
 using Domain.ResultSetsModels;
 using Domain.ViewModels;
 using Google.Apis.Sheets.v4;
@@ -21,14 +22,14 @@ namespace Infrastructure.Repositories
 {
 	public class AsistenciaRepository : GenericRepository<Asistencia>
 	{
-		//private readonly SpreadsheetsResource.ValuesResource _googleSheetValues;
+		private readonly SpreadsheetsResource.ValuesResource _googleSheetValues;
 
 		private const string SPREADSHEET_ID = "1q8x_9-tZaAyXKDxiC1FceoKwtVsGubky9QvRyeX25RU";
 		private const string SHEET_NAME = "prueba";
 
 		public AsistenciaRepository(MainContext mainContext) : base(mainContext)
 		{
-			//_googleSheetValues = new GoogleSheetHelper().Service.Spreadsheets.Values;
+			_googleSheetValues = new GoogleSheetHelper().Service.Spreadsheets.Values;
 		}
 
 		// Map Excel section
@@ -50,45 +51,43 @@ namespace Infrastructure.Repositories
 			return rangeData;
 		}
 
-		//public async Task AddNewRowToExcel(Asistencia model)
-		//{
-		//	var unidadMiembro = await _context.UnidadMiembro.FindAsync(model.UnidadMiembroId);
+		public async Task AddNewRowToExcel(Asistencia model)
+		{
+			var unidadMiembro = await _context.UnidadMiembro.FindAsync(model.UnidadMiembroId);
 
-		//	await _context.Entry(unidadMiembro).Reference(u => u.Unidad).LoadAsync();
-		//	await _context.Entry(unidadMiembro.Unidad).Reference(u => u.Tramo).LoadAsync();
-		//	await _context.Entry(unidadMiembro.Unidad.Tramo).Reference(u => u.RegionAsistencia).LoadAsync();
+			await _context.Entry(unidadMiembro).Reference(u => u.Unidad).LoadAsync();
+			await _context.Entry(unidadMiembro.Unidad).Reference(u => u.Tramo).LoadAsync();
+			await _context.Entry(unidadMiembro.Unidad.Tramo).Reference(u => u.RegionAsistencia).LoadAsync();
 
-		//	var municipio = await _context.Municipios.FindAsync(model.MunicipioId);
+			var municipio = await _context.Municipios.FindAsync(model.MunicipioId);
 
-		//	await _context.Entry(municipio).Reference(m => m.Provincia).LoadAsync();
+			await _context.Entry(municipio).Reference(m => m.Provincia).LoadAsync();
 
-		//	string tipoAsistencia = "";
+			foreach (var item in model.TipoAsistencias)
+			{
+				var excelModel = new AsistenciaExcelSheetModel()
+				{
+					Tipo = item.Nombre,
+					Categoria = item.CategoriaAsistencia.ToString(),
+					Unidad = unidadMiembro.Unidad.Denominacion,
+					Region = unidadMiembro.Unidad.Tramo.RegionAsistencia.Nombre,
+					Provincia = municipio.Provincia.Nombre,
+					Municipio = municipio.Nombre,
+					Tramo = unidadMiembro.Unidad.Tramo.Nombre,
+					Fecha = DateTime.Now.ToString("d")
+				};
 
-		//	foreach (var item in model.TipoAsistencias)
-		//	{
-		//		tipoAsistencia += $"{item.Nombre}, ";
-		//	}
+				var valueRange = new ValueRange
+				{
+					Values = MapToRangeData(excelModel)
+				};
 
-		//	var excelModel = new AsistenciaExcelSheetModel()
-		//	{
-		//		Tipo = tipoAsistencia,
-		//		Categoria = model.TipoAsistencias[0].CategoriaAsistencia.ToString(),
-		//		Unidad = unidadMiembro.Unidad.Denominacion,
-		//		Region = unidadMiembro.Unidad.Tramo.RegionAsistencia.Nombre,
-		//		Provincia = municipio.Provincia.Nombre,
-		//		Municipio = municipio.Nombre,
-		//		Tramo = unidadMiembro.Unidad.Tramo.Nombre,
-		//		Fecha = DateTime.Now.ToString("d")
-		//	};
-		//	var valueRange = new ValueRange
-		//	{
-		//		Values = MapToRangeData(excelModel)
-		//	};
+				var appendRequest = _googleSheetValues.Append(valueRange, SPREADSHEET_ID, $"{SHEET_NAME}!A:I");
+				appendRequest.ValueInputOption = AppendRequest.ValueInputOptionEnum.USERENTERED;
+				appendRequest.Execute();
+			}
 
-		//	var appendRequest = _googleSheetValues.Append(valueRange, SPREADSHEET_ID, $"{SHEET_NAME}!A:I");
-		//	appendRequest.ValueInputOption = AppendRequest.ValueInputOptionEnum.USERENTERED;
-		//	appendRequest.Execute();
-		//}
+		}
 
 		private async Task<int> GetUnidadMiembroId(int unidadId)
 		{
@@ -230,7 +229,7 @@ namespace Infrastructure.Repositories
 				asistencia.UsuarioId = (int) model.CodUsuario;
 
 				// insert to excel
-				//AddNewRowToExcel(asistencia);
+				await AddNewRowToExcel(asistencia);
 			}			
 
 			_context.Attach<Asistencia>(asistencia);
