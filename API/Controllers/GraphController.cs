21ae6a5dd00d9c5d42e639e9dbde016464f2e5a0
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-09-06 15:22:16 -0400

    graph added

---
+++ API/Controllers/GraphController.cs
@@ -0,0 +1,91 @@
+﻿using Application.DataAccess;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace API.Controllers
+{
+	[Route("api/reportes")]
+	[ApiController]
+	[Authorize]
+	public class GraphController : ControllerBase
+	{
+		private readonly IStatsRepository _statsRepository;
+		private readonly Expression<Func<Asistencia, bool>> _predicate;
+		private readonly ISpecifaction<Asistencia> _specifaction;
+		private StatsFilterDTO _filters;
+		public GraphController(IStatsRepository statsRepository, ISpecifaction<Asistencia> specifaction)
+		{
+			_specifaction = specifaction;
+			_statsRepository = statsRepository;
+			_predicate = _specifaction.GetFilterPredicate(x => (int)x.EstatusAsistencia == _filters.Estatus && x.FechaCreacion.Date >= _filters.Initial.Date && x.FechaCreacion.Date <= _filters.Final.Date);
+		}
+
+		[HttpGet("regiones")]
+		public async Task<JsonResult> GetStatsRegiones([FromQuery] StatsFilterDTO filters)
+		{
+			try
+			{
+				_filters = filters;
+				var result = await _statsRepository.GetAsistenciasByRegion(_predicate);
+				return new JsonResult(result);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+
+		[HttpGet("provincias")]
+		public async Task<JsonResult> GetStatsProvincias([FromQuery] StatsFilterDTO filters)
+		{
+			try
+			{
+				_filters = filters;
+				var result = await _statsRepository.GetAsistenciasByProvincia(_predicate);
+				return new JsonResult(result);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+
+		[HttpGet("tramos")]
+		public async Task<JsonResult> GetStatsTramos([FromQuery] StatsFilterDTO filters)
+		{
+			try
+			{
+				_filters = filters;
+				var result = await _statsRepository.GetAsistenciasByTramo(_predicate);
+				return new JsonResult(result);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+
+		[HttpGet("tipoVehiculos")]
+		public async Task<JsonResult> GetStatsTipoVehiculo([FromQuery] StatsFilterDTO filters)
+		{
+			try
+			{
+				_filters = filters;
+				var result = await _statsRepository.GetAsistenciasByTipoVehiculo(_predicate);
+				return new JsonResult(result);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+
+	}
+}
---
+++ Infrastructure/Repositories/StatsRepository.cs
@@ -0,0 +1,65 @@
+﻿using Domain.ViewModels;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+	public class StatsRepository : IStatsRepository
+	{
+		private readonly MainContext _context;
+		private readonly DbSet<Asistencia> _asistencias;
+
+		public StatsRepository(MainContext context)
+		{
+			_context = context;
+			_asistencias = _context.Set<Asistencia>();
+		}
+
+		public async Task<List<GraphViewModel>> GetAsistenciasByRegion(Expression<Func<Asistencia, bool>> predicate)
+		{
+			return await _asistencias
+				.Where(predicate)
+				.Include(x => x.UnidadMiembro.Unidad.Tramo.RegionAsistencia)
+				.GroupBy(x => x.UnidadMiembro.Unidad.Tramo.RegionAsistencia.Nombre)
+				.Select(x => new GraphViewModel { Nombre = x.Key, Value = x.Count() })
+				.ToListAsync();				
+		}
+
+		public async Task<List<GraphViewModel>> GetAsistenciasByTramo(Expression<Func<Asistencia, bool>> predicate)
+		{
+			return await _asistencias
+				.Where(predicate)
+				.Include(x => x.UnidadMiembro.Unidad.Tramo)
+				.GroupBy(x => x.UnidadMiembro.Unidad.Tramo.Nombre)
+				.Select(x => new GraphViewModel { Nombre = x.Key, Value = x.Count() })
+				.ToListAsync();
+		}
+
+		public async Task<List<GraphViewModel>> GetAsistenciasByProvincia(Expression<Func<Asistencia, bool>> predicate)
+		{
+			return await _asistencias
+				.Where(predicate)
+				.Include(x => x.Provincia)
+				.GroupBy(x => x.Provincia.Nombre)
+				.Select(x => new GraphViewModel { Nombre = x.Key, Value = x.Count() })
+				.ToListAsync();
+		}
+
+		public async Task<List<GraphViewModel>> GetAsistenciasByTipoVehiculo(Expression<Func<Asistencia, bool>> predicate)
+		{
+			return await _asistencias
+				.Where(predicate)
+				.Include(x => x.VehiculoTipo)
+				.GroupBy(x => x.VehiculoTipo.Nombre)
+				.Select(x => new GraphViewModel { Nombre = x.Key, Value = x.Count() })
+				.ToListAsync();
+		}
+
+	}
+}
